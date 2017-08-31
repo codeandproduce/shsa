f3c70c01703283e2a61df864b3434d264ec95bde
Chan Woo Kim <chanwookim@Chans-MacBook-Pro.local>
2017-08-31 10:05:49 +0800

    responsive

--- public/scripts/jq-connect.js
+++ public/scripts/jq-connect.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
-  setTimeout(function(){
-    $('ul').css('margin-top','100px');
-    $('ul').css('opacity','1');
-  },800);
+  console.log($(window).width());
+  if($(window).width()<1000){
+    setTimeout(function(){
+      $('ul').css('margin-top','40px');
+      $('ul').css('opacity','1');
+    },800);
+  }else{
+    setTimeout(function(){
+      $('ul').css('margin-top','100px');
+      $('ul').css('opacity','1');
+    },800);
+  }
+
 });
